4c72a74c982d9e32061a49fc658083b8c04372db
Hugo Ribeiro <hgribeiro@live.com>
2020-04-29 09:50:37 -0300

    pag de logon pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => <></>;
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
